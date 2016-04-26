46b57e4ecbcc612fdd1e5ccd1ffd8f4b9e920ab7
vincent.tarreau <vtarreau@student.42.fr>
2016-04-26 16:26:46 +0200

    add enum factory

---
+++ abstract_vm/OperandFactory.hpp
@@ -0,0 +1,40 @@
+// ************************************************************************** //
+//                                                                            //
+//                                                        :::      ::::::::   //
+//   OperandFactory.hpp                                 :+:      :+:    :+:   //
+//                                                    +:+ +:+         +:+     //
+//   By: vtarreau <vtarreau@student.42.fr>          +#+  +:+       +#+        //
+//                                                +#+#+#+#+#+   +#+           //
+//   Created: 2016/04/26 14:28:32 by vtarreau          #+#    #+#             //
+//   Updated: 2016/04/26 14:28:33 by vtarreau         ###   ########.fr       //
+//                                                                            //
+// ************************************************************************** //
+
+#ifndef OPERANDFACTORY_HPP
+#define OPERANDFACTORY_HPP
+
+#include <iostream>
+#include "IOperand.hpp"
+#include "eOperand.hpp"
+
+class OperandFactory {
+
+public:
+
+	OperandFactory();
+	OperandFactory(OperandFactory const & src)
+	~OperandFactory();
+
+	OperandFactory & operator=(OperandFactory & const src);
+
+	IOperand const * createOperand(eOperandType typem std::string const & value) const;
+
+	IOperand const * createInt8( std::string const & value ) const;
+	IOperand const * createInt16( std::string const & value ) const;
+	IOperand const * createInt32( std::string const & value ) const;
+	IOperand const * createFloat( std::string const & value ) const;
+	IOperand const * createDouble( std::string const & value ) const;
+	
+};
+
+#endif
