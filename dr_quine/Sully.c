71f2b7b6f22fdc34ad3b95191ed933bc50d855de
vincent.tarreau <vtarreau@student.42.fr>
2016-11-24 12:52:48 +0100

    Sully Dr Quinn ok, verifier si besoin de creer le fichier dans un repertoire sup

--- dr_quine/Sully.c
+++ dr_quine/Sully.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define S "#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#define S %c%s%c%cint main() {%c%cint i = %d;%c%cFILE *f = fopen(%cSully_%d.c%c, %cw%c);%c%ci = (strcmp(__FILE__, %cSully.c%c) == 0 ? 5 : i - 1);%c%cfprintf(f, S, 10, 10, 10, 34, S, 34, 10, 10, 9, i, 10, 9, 34, i, 34, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 34, i, i, i, 34, 10, 10);fclose(f);%c%cif (i >= 0)%c%csystem(%cclang -Werror -Wextra -Wall -o Sully_%d Sully_%d.c; ./Sully_%d%c);%c}%c"
+#define A "Sully_%d.c"
+#define F "clang -Wall -Werror -Wextra -o Sully_%d Sully_%d.c; ./Sully_%d"
+#define S "#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#define A %cSully_%cd.c%c%c#define F %cclang -Wall -Werror -Wextra -o Sully_%cd Sully_%cd.c; ./Sully_%cd%c%c#define S %c%s%c%cint main() {%c%cint i = %d;%c%ci = (strcmp(__FILE__, %cSully.c%c) == 0 ? 5 : i - 1);%c%cchar a[50];sprintf(a, A, i );FILE *f = fopen(a, %cw%c);%c%cfprintf(f, S, 10, 10, 10, 34, 37, 34, 10, 34, 37, 37, 37, 34, 10, 34, S, 34, 10, 10, 9, i, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 10);fclose(f);%c%cif (i > 0)%c%c{char s[200];sprintf(s, F, i, i, i);system(s);}%c}%c"
 int main() {
 	int i = 5;
-	FILE *f = fopen("Sully_5.c", "w");
 	i = (strcmp(__FILE__, "Sully.c") == 0 ? 5 : i - 1);
-	fprintf(f, S, 10, 10, 10, 34, S, 34, 10, 10, 9, i, 10, 9, 34, i, 34, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 34, i, i, i,34, 10, 10);fclose(f);
-	if (i >= 0)
-	system("clang -Werror -Wextra -Wall -o Sully_5 Sully_5.c; ./Sully_5");
+	char a[50];sprintf(a, A, i );FILE *f = fopen(a, "w");
+	fprintf(f, S, 10, 10, 10, 34, 37, 34, 10, 34, 37, 37, 37, 34, 10, 34, S, 34, 10, 10, 9, i, 10, 9, 34, 34, 10, 9, 34, 34, 10, 9, 10, 9, 10, 9, 10, 10);fclose(f);
+	if (i > 0)
+	{char s[200];sprintf(s, F, i, i, i);system(s);}
 }
