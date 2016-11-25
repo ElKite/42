063a8d4d45ecd4b423977afe96d86bbc1f1b274a
vincent.tarreau <vtarreau@student.42.fr>
2016-11-25 08:27:49 +0100

    Edit define main equivalent to the subject

--- dr_quine/Grace.c
+++ dr_quine/Grace.c
@@ -3,10 +3,6 @@
 	Ceci est un commentaire
 */
 #define kid "Grace_kid.c"
-#define S "#include <stdio.h>%c/*%c%cCeci est un commentaire%c*/%c#define kid %cGrace_kid.c%c%c#define S %c%s%c%c#define start main%c%cint start() {%c%cFILE *f = fopen(kid, %cw%c);%c%cfprintf(f, S, 10, 10, 9, 10, 10, 34, 34, 10, 34, S, 34, 10, 10, 10, 10, 9, 34, 34, 10, 9, 10, 10);%c}%c"
-#define start main
-
-int start() {
-	FILE *f = fopen(kid, "w");
-	fprintf(f, S, 10, 10, 9, 10, 10, 34, 34, 10, 34, S, 34, 10, 10, 10, 10, 9, 34, 34, 10, 9, 10, 10);
-}
+#define S "#include <stdio.h>%c/*%c%cCeci est un commentaire%c*/%c#define kid %cGrace_kid.c%c%c#define S %c%s%c%c#define start() int main() {FILE *f = fopen(kid, %cw%c);fprintf(f, S, 10, 10, 9, 10, 10, 34, 34, 10, 34, S, 34, 10, 34, 34, 10, 10);}%cstart();%c"
+#define start() int main() {FILE *f = fopen(kid, "w");fprintf(f, S, 10, 10, 9, 10, 10, 34, 34, 10, 34, S, 34, 10, 34, 34, 10, 10);}
+start();
