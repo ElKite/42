0ea9c8771206d2720e954a3232ced9587bece6f5
vincent.tarreau <vtarreau@student.42.fr>
2016-11-25 09:33:55 +0100

    edit to call a function from main

--- dr_quine/Colleen.c
+++ dr_quine/Colleen.c
@@ -3,9 +3,9 @@
 /*
 	This function is useless
 */
-void im_useless()
+void im_useless(char * program)
 {
-
+	printf(program, 10, 10, 10, 9, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, program, 34, 10, 9, 10, 10);
 }
 
 int main()
@@ -13,7 +13,6 @@ int main()
 	/*
 		I'm a comment
 	*/
-	char *program = "#include <stdio.h>%c%c/*%c%cThis function is useless%c*/%cvoid im_useless()%c{%c%c}%c%cint main()%c{%c%c/*%c%c%cI'm a comment%c%c*/%c%cchar *program = %c%s%c;%c%cprintf(program, 10, 10, 10, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 10 , 9, 9, 10, 9, 10, 9, 34, program, 34, 10, 9, 10, 9, 10, 10);%c%creturn 0;%c}%c";
-	printf(program, 10, 10, 10, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 10 , 9, 9, 10, 9, 10, 9, 34, program, 34, 10, 9, 10, 9, 10, 10);
+	char *program = "#include <stdio.h>%c%c/*%c%cThis function is useless%c*/%cvoid im_useless(char * program)%c{%c%cprintf(program, 10, 10, 10, 9, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 34, program, 34, 10, 9, 10, 10);%c}%c%cint main()%c{%c%c/*%c%c%cI'm a comment%c%c*/%c%cchar *program = %c%s%c; im_useless(program);%c%creturn 0;%c}%c"; im_useless(program);
 	return 0;
 }
